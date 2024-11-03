edf1b022fef227c73d633a647620e7aae183372c
Ruben Ferreira <rf5885@student.uni-lj.si>
2024-11-03 13:20:26 +0100

    Initial app/security config

---
+++ src/main/java/si/uni/prpo/group03/venueservice/config/AppConfig.java
@@ -0,0 +1,14 @@
+package si.uni.prpo.group03.venueservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
